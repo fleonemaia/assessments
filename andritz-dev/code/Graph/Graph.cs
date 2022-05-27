f51a29b8752652db5fb15158fe6e7c781de7af4d
fleonemaia <fleonemaia@gmail.com>
2022-05-27 15:59:18 -0300

    Working Graph

--- andritz-dev/code/Graph/Graph.cs
+++ andritz-dev/code/Graph/Graph.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace Graph
 {
@@ -10,14 +14,39 @@ namespace Graph
 
     public class Graph<T> : IGraph<T>
     {
+        private readonly IEnumerable<ILink<T>> Links;
+
         public Graph(IEnumerable<ILink<T>> links)
         {
-
+            Links = links;
         }
 
         public IObservable<IEnumerable<T>> RoutesBetween(T source, T target)
         {
-            throw new NotImplementedException();
+            return Observable.Create<IEnumerable<T>>(observer =>
+            {
+                var possibleSources = Links.Where(e => e.Source.Equals(source));
+                var pathsTaken = new List<T>();
+
+                foreach (var item in possibleSources)
+                {
+                    var temporarySource = item.Source;
+                    var paths = new List<T>();
+                    
+                    while (!target.Equals(temporarySource))
+                    {
+                        var nextTarget = Links.Where(e => e.Source.Equals(temporarySource) && !pathsTaken.Contains(e.Target)).FirstOrDefault();
+                        paths.Add(temporarySource);
+                        pathsTaken.Add(temporarySource);
+                        temporarySource = nextTarget.Target;
+                    }
+
+                    paths.Add(temporarySource);
+                    observer.OnNext(paths);
+                }
+                observer.OnCompleted();
+                return Disposable.Empty;
+            });
         }
     }
 }
